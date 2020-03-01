ea62bb3d53f7418468a38e1a80ffee0c75936419
Kanmarina <kanmarina.coach@yahoo.com>
2020-03-01 12:34:39 -0600

    first commit

--- src/test/java/com/automation/tests/day1/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day1/day2/BasicNavigation.java
@@ -4,12 +4,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
 
         driver.get("http://google.com");
-
+        Thread.sleep(3000);
+        driver.close();
 
 
     }
